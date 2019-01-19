fe4a2ff2597fead1034fa6d2f75c9a2ee256da6f
Thomas Hallgren <thomas@tada.se>
2019-01-19 13:47:33 +0100

    (#45) Add delete command and calls to garbage collector
    This commit adds the command `delete` and ensures that the current
    `apply` command makes use of the garbage collector.
    
    closes #45

--- cmd/lyra/cmd/delete.go
+++ cmd/lyra/cmd/delete.go
@@ -12,22 +12,18 @@ import (
 	_ "github.com/lyraproj/hiera/functions"
 )
 
-var homeDir string
-var hieraDataFilename string
-
-// NewApplyCmd returns the apply subcommand used to evaluate and apply activities. //TODO: (JD) Does 'apply' even make sense for what this does now?
-func NewApplyCmd() *cobra.Command {
+// NewDeleteCmd returns the delete subcommand used to delete activities.
+func NewDeleteCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     i18n.T("applyCmdUse"),
-		Short:   i18n.T("applyCmdShort"),
-		Long:    i18n.T("applyCmdLong"),
-		Example: i18n.T("applyCmdExample"),
-		Run:     runApplyCmd,
+		Use:     i18n.T("deleteCmdUse"),
+		Short:   i18n.T("deleteCmdShort"),
+		Long:    i18n.T("deleteCmdLong"),
+		Example: i18n.T("deleteCmdExample"),
+		Run:     runDeleteCmd,
 		Args:    cobra.ExactArgs(1),
 	}
 
 	cmd.Flags().StringVarP(&homeDir, "root", "r", "", i18n.T("flagHomeDir"))
-	cmd.Flags().StringVarP(&hieraDataFilename, "data", "d", "data.yaml", i18n.T("applyFlagExtData"))
 
 	cmd.SetHelpTemplate(ui.HelpTemplate)
 	cmd.SetUsageTemplate(ui.UsageTemplate)
@@ -35,10 +31,10 @@ func NewApplyCmd() *cobra.Command {
 	return cmd
 }
 
-func runApplyCmd(cmd *cobra.Command, args []string) {
+func runDeleteCmd(cmd *cobra.Command, args []string) {
 	applicator := &apply.Applicator{HomeDir: homeDir}
 	workflowName := args[0]
-	exitCode := applicator.ApplyWorkflow(workflowName, hieraDataFilename, wfapi.Upsert)
+	exitCode := applicator.ApplyWorkflow(workflowName, hieraDataFilename, wfapi.Delete)
 	if exitCode != 0 {
 		os.Exit(exitCode)
 	}
